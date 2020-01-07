2253473ff0cc233a214983855963a7a61fb07cd0
Maxim Kuznetsov <spoket@iMax.local>
2020-01-07 20:58:39 +0400

    Remove non used packages from install

--- packages/setup.sh
+++ packages/setup.sh
@@ -9,20 +9,21 @@ COMMENT=\#*
 
 sudo -v
 
-info "Installing Brewfile packages..."
+info "Installing Brewfile packages:"
 brew bundle
-success "Finished installing Brewfile packages."
 
 find * -name "*.list" | while read fn; do
     cmd="${fn%.*}"
     set -- $cmd
-    info "Installing $1 packages..."
+    info "Installing $1 package..."
     while read package; do
         if [[ $package == $COMMENT ]];
         then continue
         fi
-        substep_info "Installing $package..."
+        substep_info "Installing $package:"
         $cmd $package
     done < "$fn"
-    success "Finished installing $1 packages."
+    success "Finished installing $1 package."
 done
+
+success "Finished installing Brewfile packages."
