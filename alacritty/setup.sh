55cea820d1b3e0f60623717d0deebdc6e41c4939
MaximumQuiet <lol8funny@gmail.com>
2020-01-08 13:57:37 +0400

    Fix configuration logging for alacritty

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -6,11 +6,13 @@ cd "$DIR"
 SOURCE="$(realpath .)"
 DESTINATION="$(realpath ~/)"
 
+info "Configuring Alacritty term"
+
+substep_info "Creating config directory for alacritty"
 mkdir -p "$DESTINATION/.config/alacritty"
 DESTINATION="$(realpath $DESTINATION/.config/alacritty)"
+substep_success "Alacritty directory is created"
 
-info "Setting up Alacritty term..."
-
+substep_info "Linking configuration file"
 symlink "$SOURCE/alacritty.yml" "$DESTINATION/alacritty.yml"
-
 success "Finished setting up Alacritty."
