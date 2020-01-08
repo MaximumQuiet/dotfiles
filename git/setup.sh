fd27ed48a9a340418525cf4a9b96e741cd00c4e2
MaximumQuiet <lol8funny@gmail.com>
2020-01-08 14:58:40 +0400

    Fix configuration logging for git

--- git/setup.sh
+++ git/setup.sh
@@ -8,7 +8,7 @@ cd "$DIR"
 SOURCE="$(realpath .)"
 DESTINATION="$(realpath ~)"
 
-info "Configuraing git..."
+info "Setting up git"
 
 find . -name ".git*" | while read fn; do
     fn=$(basename $fn)
